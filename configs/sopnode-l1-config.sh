1105b4bc7b2d51fb2a9f1fa8e6af763942e95981
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2022-04-05 16:47:34 +0200

    add config for sopnode-l1

---
+++ configs/sopnode-l1-config.sh
@@ -0,0 +1,7 @@
+K8S_CLUSTER_NAME=prod
+K8S_VERSION=1.22.4
+K8S_API_ENDPOINT=sopnode-l1.inria.fr
+K8S_API_ENDPOINT_INTERNAL=sopnode-l1.inria.fr
+K8S_API_ADDVERTISE_IP_1=138.96.245.50
+
+MASTER_SSH_ADDR_1=root@sopnode-l1.inria.fr
