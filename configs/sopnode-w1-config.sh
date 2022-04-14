64f78a16af7dd499ebe434bf2d6c75d25cf7d7e0
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2022-04-14 10:42:57 +0200

    config for w1

---
+++ configs/sopnode-w1-config.sh
@@ -0,0 +1,7 @@
+K8S_CLUSTER_NAME=devel-w1
+K8S_VERSION=1.22.4
+K8S_API_ENDPOINT=sopnode-w1.inria.fr
+K8S_API_ENDPOINT_INTERNAL=sopnode-w1.inria.fr
+K8S_API_ADDVERTISE_IP_1=138.96.245.51
+
+MASTER_SSH_ADDR_1=root@sopnode-w1.inria.fr
