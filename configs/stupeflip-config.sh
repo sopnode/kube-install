ef5e0820340b3b9e3073cffc63d5babaab8b7952
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2022-12-01 15:58:44 +0100

    a config file for the stupeflip cluster

---
+++ configs/stupeflip-config.sh
@@ -0,0 +1,6 @@
+K8S_CLUSTER_NAME=diana
+K8S_API_ENDPOINT=stupeflip.pl.sophia.inria.fr
+K8S_API_ENDPOINT_INTERNAL=stupeflip.pl.sophia.inria.fr
+K8S_API_ADDVERTISE_IP_1=138.96.112.34
+
+MASTER_SSH_ADDR_1=root@stupeflip.pl.sophia.inria.fr
