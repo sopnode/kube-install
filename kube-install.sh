c459d2f1cb5eb477163097907db80b396b3c7fff
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2023-01-30 16:45:08 +0100

    cosmetic change in the prepare phase

--- kube-install.sh
+++ kube-install.sh
@@ -98,7 +98,7 @@ EOF
 
   cat <<EOF > /etc/sysctl.d/k8s.conf
 net.ipv4.ip_forward=1
-net.bridge.bridge-nf-call-iptables = 1
+net.bridge.bridge-nf-call-iptables=1
 EOF
     sysctl -p /etc/sysctl.d/k8s.conf
 }
