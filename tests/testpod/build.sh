58c56b8b101d8de2ebf5ff50c20002c7d158fab6
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2023-02-13 19:04:25 +0100

    fix tests
    the script building the test pod image was broken

--- tests/testpod/build.sh
+++ tests/testpod/build.sh
@@ -6,15 +6,17 @@ function build-if-not-present() {
     local image="$1"; shift
     local dir="$1"; shift
     local image_exists=""
-    crictl inspecti lcocalhost/$image >& /dev/null || image_exists=true
+    crictl inspecti localhost/$image >& /dev/null && image_exists=true
     [[ -n "$image_exists" && -z "$FORCE" ]] && {
         echo $image already present - ignored
         return
     }
-    buildah build -t $image $dir
+    # we used to do buildah build but it seems outdated
+    buildah bud -t $image $dir
 }
 
 [[ "$1" == "--force" ]] && FORCE=true
 
-build-if-not-present fedora-with-ping fping
+# the tests use only the ubuntu image
+#build-if-not-present fedora-with-ping fping
 build-if-not-present ubuntu-with-ping uping
